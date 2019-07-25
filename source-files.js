2c236c1bb17946f8c45bf5a8b6a91661edc7ce56
ci-build <ci-build@hyperbo.la>
2019-07-25 02:46:51 +0000

    [skip ci] generate docs

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["cactusref"] = {"name":"","dirs":[{"name":"cycle","files":["drop.rs","mod.rs"]}],"files":["adoptable.rs","lib.rs","link.rs","ptr.rs","rc.rs","weak.rs"]};
+sourcesIndex["cactusref"] = {"name":"","files":["adoptable.rs","cyclic.rs","drop.rs","lib.rs","link.rs","ptr.rs","rc.rs","weak.rs"]};
 createSourceSidebar();
