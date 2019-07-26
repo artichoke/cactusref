c2c62d184e150133e52046e196bddd06e4792367
Ryan Lopopolo <rjl@hyperbo.la>
2019-07-26 01:03:16 -0700

    Convert liballoc/rc integration tests to cactusref
    Remove tests for unsized types since cactusref does not implement CoerceUnsized or Rc<Any>

--- tests/rc.rs
+++ tests/rc.rs
@@ -1,7 +1,7 @@
+use cactusref::{Rc, Weak};
 use std::any::Any;
 use std::cell::RefCell;
 use std::cmp::PartialEq;
-use std::rc::{Rc, Weak};
 
 #[test]
 fn uninhabited() {
@@ -9,41 +9,6 @@ fn uninhabited() {
     let mut a = Weak::<Void>::new();
     a = a.clone();
     assert!(a.upgrade().is_none());
-
-    let mut a: Weak<dyn Any> = a; // Unsizing
-    a = a.clone();
-    assert!(a.upgrade().is_none());
-}
-
-#[test]
-fn slice() {
-    let a: Rc<[u32; 3]> = Rc::new([3, 2, 1]);
-    let a: Rc<[u32]> = a; // Unsizing
-    let b: Rc<[u32]> = Rc::from(&[3, 2, 1][..]); // Conversion
-    assert_eq!(a, b);
-
-    // Exercise is_dangling() with a DST
-    let mut a = Rc::downgrade(&a);
-    a = a.clone();
-    assert!(a.upgrade().is_some());
-}
-
-#[test]
-fn trait_object() {
-    let a: Rc<u32> = Rc::new(4);
-    let a: Rc<dyn Any> = a; // Unsizing
-
-    // Exercise is_dangling() with a DST
-    let mut a = Rc::downgrade(&a);
-    a = a.clone();
-    assert!(a.upgrade().is_some());
-
-    let mut b = Weak::<u32>::new();
-    b = b.clone();
-    assert!(b.upgrade().is_none());
-    let mut b: Weak<dyn Any> = b; // Unsizing
-    b = b.clone();
-    assert!(b.upgrade().is_none());
 }
 
 #[test]
