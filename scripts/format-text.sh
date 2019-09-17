232238b4cdb4287324bb1c721d2013745e5bbd28
Ryan Lopopolo <rjl@hyperbo.la>
2019-09-16 23:05:26 -0700

    Explicitly set parser in format-text.sh

--- scripts/format-text.sh
+++ scripts/format-text.sh
@@ -15,23 +15,36 @@ wrap() {
   fi
 }
 
+parser() {
+  if [[ $1 == "js" ]]; then
+    echo "--parser babel"
+  elif [[ $1 == "md" ]]; then
+    echo "--parser markdown"
+  elif [[ $1 == "yml" ]]; then
+    echo "--parser yaml"
+  else
+    echo "--parser $1"
+  fi
+}
+
 format() {
   # shellcheck disable=SC2046
   find . -type f \
     -and -name "*.$1" \
-    -and -not -path '*target*' \
-    -and -not -path '*node_modules*' -print0 |
-    xargs -0 yarn run prettier --write $(wrap "$1")
+    -and -not -path '*vendor/*/*' \
+    -and -not -path '*target/*' \
+    -and -not -path '*node_modules/*' -print0 |
+    xargs -0 yarn run prettier $(parser "$1") --write $(wrap "$1")
 }
 
 check() {
   # shellcheck disable=SC2046
   find . -type f \
     -and -name "*.$1" \
-    -and -not -path '*target*' \
-    -and -not -path '*node_modules*' \
-    -and -not -path '*node_modules*' -print0 |
-    xargs -0 yarn run prettier --check $(wrap "$1")
+    -and -not -path '*vendor/*/*' \
+    -and -not -path '*target/*' \
+    -and -not -path '*node_modules/*' -print0 |
+    xargs -0 yarn run prettier $(parser "$1") --check $(wrap "$1")
 }
 
 if [[ $# -gt 1 && $1 == '--check' ]]; then
