d24c9e15b1acc87154f39a1c2d5fb4e714c4bacf
artichoke-ci <ci@artichokeruby.org>
2021-06-14 04:56:35 +0000

    deploy: be402511382f0d995e2049cbce83deb2cbbf6dbe

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["cactusref","cfg_if","hashbrown","log","rustc_hash"];
--- source-files.js
+++ source-files.js
@@ -1,3 +1,7 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["cactusref"] = {"name":"","files":["adoptable.rs","cyclic.rs","drop.rs","lib.rs","link.rs","ptr.rs","rc.rs","weak.rs"]};
+sourcesIndex["cactusref"] = {"name":"","dirs":[{"name":"doc","files":["implementing_self_referential_data_structures.rs"]}],"files":["adopt.rs","cycle.rs","drop.rs","hash.rs","lib.rs","link.rs","rc.rs"]};
+sourcesIndex["cfg_if"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["hashbrown"] = {"name":"","dirs":[{"name":"external_trait_impls","files":["mod.rs"]},{"name":"raw","files":["alloc.rs","bitmask.rs","mod.rs","sse2.rs"]}],"files":["lib.rs","macros.rs","map.rs","scopeguard.rs","set.rs"]};
+sourcesIndex["log"] = {"name":"","files":["lib.rs","macros.rs"]};
+sourcesIndex["rustc_hash"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
